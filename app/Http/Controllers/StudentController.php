7623e8fed9126de3e300b4b026cf86090ea716c7
CHriss <christophervistal25@gmail.com>
2020-01-22 13:25:53 +0800

    add token after register in api

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StudentController extends Controller
 {
@@ -15,16 +16,56 @@ class StudentController extends Controller
 
     public function store(Request $request) :Student
     {
-    	return Student::firstOrCreate(
+    	 $student = Student::firstOrCreate(
     		[
     			'id_number' => $request->id_number
     		],
     		$request->all()
     	);
+        if (! $token = Auth::attempt(['id_number' => $student->id_number, 'password' => $student->password] )) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }          
+         return $this->respondWithToken($token);
     }
 
     public function show(int $id) : Student
     {
     	return Student::find($id);
     }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+     /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+     protected function respondWithToken($token)
+     {
+        return response()->json([
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => Auth::factory()->getTTL() * 60
+        ], 200);
+     }
 }
